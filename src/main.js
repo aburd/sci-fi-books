43a3fa1592efc79cdcb5d027adb3247ad9b8422b
aburd <burdick.aaron@gmail.com>
2022-03-31 20:08:04 +0900

    refactor: extract fn to module

--- src/main.js
+++ src/main.js
@@ -1,20 +1,4 @@
-// Load in the file-system module from the node standard-library
-// Whenever anybody talks about a "standard library", they're just talking'
-// about a library of code that the creators of the language give you
-// to make the code more useful
-// https://nodejs.org/dist/latest-v16.x/docs/api/fs.html
-const fs = require('fs');
-
-/**
- * A function that gets our books, could connect to a DB in the future
- * For now, just reads a JSON file
- * @return {Book[]}
- */
-function getScifiBooks() {
-  const dbPath = `${__dirname}/fake-db.json`;
-  const books = fs.readFileSync(dbPath, { encoding: 'utf8' });
-  return JSON.parse(books);
-}
+const { getScifiBooks } = require('./db');
 
 /**
  * Displays the books, for now we will just print the books
