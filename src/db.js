7deadaf826d57a3cfbc14a602b562ac774763d4a
aburd <burdick.aaron@gmail.com>
2022-04-06 20:27:28 +0900

    feat: use new model and check to see that book not in db before possibly adding

--- src/db.js
+++ src/db.js
@@ -1,3 +1,10 @@
+// Code dealing with getting our books from a data source
+// 
+
+/**
+ * @typedef {import('./book').Book} Book
+ */
+
 // Load in the file-system module from the node standard-library
 // Whenever anybody talks about a "standard library", they're just talking'
 // about a library of code that the creators of the language give you
@@ -12,19 +19,41 @@ const DB_PATH = `${__dirname}/fake-db.json`;
  * For now, just reads a JSON file
  * @return {Book[]}
  */
-exports.getScifiBooks = async function () {
+async function getScifiBooks() {
   const books = await fs.readFile(DB_PATH, { encoding: "utf8" });
   return JSON.parse(books);
 };
 
 /**
+ * Get book from db by some ISBN
+ * @param {string} isbn
+ * @return {Book || null}
+ */
+async function getScifiBook(isbn) {
+  const books = await getScifiBooks();
+  for (const book of books) {
+    if (book.isbn10 == isbn || book.isbn13 == isbn) {
+      return book;
+    }
+  }
+  return null;
+};
+
+/**
  * A function that adds a book to our DB
  * For now, just adds book to the JSON file
- * @return {Book[]}
+ * @param {Book} book
+ * @return {void} 
  */
-exports.addScifiBook = async function (book) {
+async function addScifiBook(book) {
   const fileContent = await fs.readFile(DB_PATH, { encoding: "utf8" });
   const books = JSON.parse(fileContent);
   books.push(book);
   await fs.writeFile(DB_PATH, JSON.stringify(books), { encoding: 'utf8' });
+};
+
+module.exports = {
+  getScifiBooks,
+  getScifiBook,
+  addScifiBook,
 };
--- src/main.js
+++ src/main.js
@@ -1,15 +1,15 @@
-const { getScifiBooks } = require('./db');
-// Dependency installed via `npm i cli-select`
-// https://www.npmjs.com/package/cli-select
-const cliSelect = require('cli-select');
+const { getScifiBooks, getScifiBook } = require("./db");
+const cliSelect = require("cli-select");
 // Dependency installed via `npm i node-fetch`
-const fetch = require('node-fetch');
+const fetch = require("node-fetch");
+const readline = require("readline");
+const { displayBook, openLibraryToBook } = require("./book");
 
 const MENU_OPTIONS = {
-  show: 'Show a book',
-  add: 'Add a Book',
-  delete: 'Delete a Book',
-  quit: 'Quit',
+  show: "Show a book",
+  add: "Add a Book",
+  delete: "Delete a Book",
+  quit: "Quit",
 };
 const cliOptions = {
   values: MENU_OPTIONS,
@@ -23,60 +23,69 @@ const cliOptions = {
 async function displayBooks() {
   const books = await getScifiBooks();
   for (const book of books) {
-    console.log(`- ${book.title}`);
+    displayBook(book);
   }
 }
 
 /**
  * A function that gets ISBN from user input
- * 
+ * @return {void}
  */
 async function regByISBN() {
-  
-  const readline = require("readline");
-  
+  const isbn = await askForIsbn();
+  const book = await getScifiBook(isbn);
+  if (book) {
+    console.log("This book is already registered");
+    displayBook(book);
+    return;
+  }
+  await printBookDetails(isbn);
+}
+
+/**
+ * Ask user for the ISBN
+ * @return {string} isbn
+ */
+async function askForIsbn() {
   const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout,
   });
-    
-    const isbn = await new Promise((isbn) => {
-      rl.question("Enter book ISBN: ", isbn);
-    });
-    rl.close();
-    
-    printBookDetails(isbn);
-    
-  }
-  
-  /**
-   * A function that uses isbn to fetch and print book details
-   * 
-   */
-  async function printBookDetails(isbn) {
-  
-    console.log(`\nRegistering ISBN: ${isbn}`);
-  
-    const openLibraryURL = 'https://openlibrary.org';
-    const bookEndPoint = '/isbn/';
-    const apiSuffix = '.json';
-    //const sampleISBN = '9780316212366'
-  
-    const bookURL = openLibraryURL + bookEndPoint + isbn + apiSuffix;
-    const bookResponse = await fetch(bookURL);
-    const book = await bookResponse.json();
-    const bookTitle = book.title;
-  
-    const authorEndPointWithID = book.authors[0].key;
-    const authorURL = openLibraryURL + authorEndPointWithID + apiSuffix;
-    const authorResponse = await fetch(authorURL);
-    const author = await authorResponse.json();
-    const authorName = author.name;
-    
-    console.log('\nTitle:', bookTitle);
-    console.log('Author:', authorName);
-    console.log('***Your book has not been added to the database, but will in the future \n \n \n')
-  }
+  const isbn = await new Promise((isbn) => {
+    rl.question("Enter book ISBN: ", isbn);
+  });
+  rl.close();
+  return isbn;
+}
+
+/**
+ * A function that uses isbn to fetch and print book details
+ *
+ */
+async function printBookDetails(isbn) {
+  //const sampleISBN = '9780316212366'
+  console.log(`\nRegistering ISBN: ${isbn}`);
+
+  const openLibraryURL = "https://openlibrary.org";
+  const bookEndPoint = "/isbn/";
+  const apiSuffix = ".json";
+
+  const bookURL = openLibraryURL + bookEndPoint + isbn + apiSuffix;
+  const bookResponse = await fetch(bookURL);
+  const openLibBook = await bookResponse.json();
+
+  const authorEndPointWithID = openLibBook.authors[0].key;
+  const authorURL = openLibraryURL + authorEndPointWithID + apiSuffix;
+  const authorResponse = await fetch(authorURL);
+  const openLibAuthor = await authorResponse.json();
+
+  const book = openLibraryToBook(openLibBook, openLibAuthor);
+  displayBook(book);
+
+  console.log(
+    "***Your book has not been added to the database, but will in the future \n \n \n"
+  );
+}
 
 /**
  * Handles the selected CLI option
@@ -84,48 +93,44 @@ async function regByISBN() {
  * @param {string} value - e.g. 'Show a Book'
  * @returns {void}
  */
- async function handleSelection({ id, value }) {
+async function handleSelection({ id, value }) {
   console.log(`>> ${value}`);
   switch (id) {
-    case 'show': {
+    case "show": {
       await displayBooks();
       break;
     }
-    case 'delete': {
-      console.log('Not supported!');
+    case "delete": {
+      console.log("Not supported!");
       break;
     }
-    case 'add': {
+    case "add": {
       await regByISBN();
       break;
     }
-    case 'quit': {
-      console.log('See ya around, space cowboy...');
+    case "quit": {
+      console.log("See ya around, space cowboy...");
       // Exit node process with exit code 0 ('success');
       // https://shapeshed.com/unix-exit-codes/
       process.exit(0);
     }
     default:
-      console.log('Unrecognized option selected');
+      console.log("Unrecognized option selected");
   }
 
-  console.log('');
+  console.log("");
   // Run menu again, 'quit' is the only way to get out of loop
   runOptionsMenu();
-  
 }
 
 // TODO: handle errors more gracefully
 function handleError(e) {
   console.error(e);
-  console.log('Something bad happened.');
+  console.log("Something bad happened.");
 }
 
-
 function runOptionsMenu() {
-  cliSelect(cliOptions)
-    .then(handleSelection)
-    .catch(handleError);
+  cliSelect(cliOptions).then(handleSelection).catch(handleError);
 }
 
 function main() {
